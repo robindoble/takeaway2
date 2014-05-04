8face3552e83289d5e83f3f21df7c29ded6d45b6
Robin <robindoble@gmail.com>
2014-05-04 18:26:57 +0100

    a new order instantiates a new instance of the Menu class (which for now has a fixed menu). I have set this up as a separate class as technically the takeaway should be able to create a new menu

--- lib/order.rb
+++ lib/order.rb
@@ -1,15 +1,20 @@
 require_relative 'takeaway'
+require_relative 'menu'
+
+
 
 class Order
 
 attr_reader :user
+attr_reader :menu
+
 # attr_accessor :takeaway
 
 
 def initialize(user)
 	@user = user
 	# @takeaway = self.class.superclass
-	# @menu = Menu.new
+	@menu = Menu.new.menu
 end
 
 # def show_menu
