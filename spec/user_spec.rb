08a01b83442a7b26eca07c09d364a26666e3f6b5
Robin <robindoble@gmail.com>
2014-05-04 16:11:32 +0100

    new order method added for user class

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,8 +4,9 @@ describe User do
 
 	it 'should be able to place an order' do
 		user=User.new
-		expect(user).to receive(:new_order)
-		user.new_order
+		expect(user.respond_to?(:new_order)).to be true
+		# expect(user).to receive(:new_order)
+		# user.new_order
 	end
 
 
